6f8ea2b8ee842ead68a349a528b5cbb4a034360a
kelvin kai <kaikevin2652gmail.com>
2023-11-06 11:54:02 -0800

    added a crying symbol in runes dart

--- DART lesson/runes.dart
+++ DART lesson/runes.dart
@@ -2,9 +2,12 @@ void main(){
   //for emojis
   var heart_symbol ='\u2665';
   var laugh_symbol ='\u{1f600}';
-  var cry_symbol ='\u{1f611}';
+  var sad_symbol ='\u{1f611}';
+var crying_symbol ='\u{1f62D}';
+
   print(heart_symbol);
   print(laugh_symbol);
-  print(cry_symbol);
+  print(sad_symbol);
+  print(crying_symbol);
 
 }
